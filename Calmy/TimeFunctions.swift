700d09f5dbba3945c5fb452da9a57645d5b3ec34
Nikita Oltyan <nikitaoltyan@mail.ru>
2021-03-09 19:11:29 +0300

    Rounding added

--- Calmy/TimeFunctions.swift
+++ Calmy/TimeFunctions.swift
@@ -12,11 +12,13 @@ import UIKit
 class TimeFunctions {
     
     /// - returns: Proportion of passed minutes in this day per all minutes in day.
+    /// - warning: Rounded to 3 decimal digits (0.001298 -> 0.001 for example)
     static func GetTime() -> CGFloat{
         let date = Date()
         let calendar = Calendar.current
         let hours = calendar.component(.hour, from: date)
         let minutes = calendar.component(.minute, from: date)
-        return CGFloat(hours*60 + minutes)/1440
+        let ret = CGFloat(hours*60 + minutes)/1440
+        return CGFloat(round(1000*ret)/1000)
     }
 }
